f2a197ebbeffd953f8b84d0215185ceb5585b9fc
BurnsD <Dustinburnsactech@gmail.com>
2022-06-22 21:44:07 -0500

    add js logic for symbols and initial values

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -1,5 +1,6 @@
 const display = document.querySelector('.display')
 const controlButtons = document.querySelector('.controls').children
+const allSymbols = ['+', '-', 'X', '÷', '%', 'C', '=']
 
 let firstValue = ''
 let secondValue = ''
@@ -8,7 +9,18 @@ let symbol = ''
 for (let button of controlButtons) {
     button.addEventListener('click', () => {
       const { innerText: btnValue } = button
+      const btnValueIsSymbol = allSymbols.includes(btnValue)
+
+    //   Used to add negative value to initial input
+      if  (firstValue && btnValueIsSymbol) {
+        symbol = btnValue
+      }
+    //   Used to find value of first input without symbol
+      else if (!symbol) firstValue += btnValue
+    //   input of symbol initiates second value
+      else if (symbol) secondValue += btnValue
+
+      
       display.innerText += btnValue
-  
     })
   }
