689eea45aab26f0fe3993ba68ebb61b7ccc603ca
BurnsD <Dustinburnsactech@gmail.com>
2022-06-22 23:16:18 -0500

    starting new js approach with data tags

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -1,27 +1,7 @@
-const display = document.querySelector('.display')
-const controlButtons = document.querySelector('.controls').children
-const allSymbols = ['+', '-', 'X', '÷', '%', 'C', '=']
-
-let firstValue = ''
-let secondValue = ''
-let symbol = ''
-
-for (let button of controlButtons) {
-    button.addEventListener('click', () => {
-      const { innerText: btnValue } = button
-      const btnValueIsSymbol = allSymbols.includes(btnValue)
-
-    //   Used to add negative value to initial input
-      if  (firstValue && btnValueIsSymbol) {
-        symbol = btnValue
-      }
-    //   Used to find value of first input without symbol
-      else if (!symbol) firstValue += btnValue
-    //   input of symbol initiates second value
-      else if (symbol) secondValue += btnValue
-
-
-      if (btnValue !== '=') display.innerText += btnValue
-    })
-  }
-
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous]')
+const currentOperandTextElement = document.querySelector('[data-current]')
